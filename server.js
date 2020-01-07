d2509bf01cb80287b2c3703c209ecf9a05b54528
Taylor Bruno <taylorbruno895@gmail.com>
2020-01-07 15:43:28 -0500

    adding heroku for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 let raw = fs.readFileSync('db/db.json');
 let notes = JSON.parse(raw);
